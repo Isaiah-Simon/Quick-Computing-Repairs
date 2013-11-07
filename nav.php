55c507f52681bd7690ab8438a17096f4a1d8c242
Isaiah Simon <isimon@okapi.uvm.edu>
2013-11-07 18:40:41 -0500

    Changed nav list for specific project

--- nav.php
+++ nav.php
@@ -1,6 +1,9 @@
 <nav class="bgColor">
 <ul>
    <li><a href='main.php'>Home</a></li>
-   <li><a href='form.php'>Survey Form</a></li>
+   <li><a href='about.php'>About Us</a></li>
+   <li><a href='services.php'>Offered Services</a></li>
+   <li><a href='serviceForm.php'>Service Form</a></li>
+   <li><a href='status.php'>Check Your Issue Status</a></li>
 </ul>
 </nav>

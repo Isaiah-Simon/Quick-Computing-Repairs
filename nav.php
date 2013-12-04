7f75d9bcb40cbf896b876ec7ae1aa679f7587332
Isaiah Simon <isimon@okapi.uvm.edu>
2013-12-04 03:02:42 -0500

    Added correct links to all my web pages

--- nav.php
+++ nav.php
@@ -3,7 +3,8 @@
    <li><a href='main.php'>Home</a></li>
    <li><a href='about.php'>About Us</a></li>
    <li><a href='services.php'>Offered Services</a></li>
-   <li><a href='serviceForm.php'>Service Form</a></li>
-   <li><a href='status.php'>Check Your Issue Status</a></li>
+   <li><a href='serviceform.php'>Service Form</a></li>
+   <li><a href='search.php'>Check Your Issue Status</a></li>
+   <li><a href='admin.php'>Admin Area</a></li>
 </ul>
 </nav>

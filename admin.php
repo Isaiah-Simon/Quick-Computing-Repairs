ddf0509d1916c88d10ae216b7d11282c05fc0700
Isaiah Simon <isimon@okapi.uvm.edu>
2013-12-03 21:55:55 -0500

    Created page that links to different admin options

---
+++ admin.php
@@ -0,0 +1,20 @@
+<? include ("top.php"); ?>
+
+<body id="admin">
+
+<section id="bgColor">
+<? include ("header.php"); ?>
+<? include ("nav.php"); ?>
+
+<section id="content" class="noBorder">
+	<ul>
+		<li><a href="http://www.uvm.edu/~isimon/cs148/assignment7.1/adminAdd.php">Add a new record</a></li>
+		<li><a href="http://www.uvm.edu/~isimon/cs148/assignment7.1/adminSearch.php">Search for an existing record.</a></li>
+	
+</section>
+
+<? include ("footer.php"); ?>
+
+</section>
+</body>
+</html>

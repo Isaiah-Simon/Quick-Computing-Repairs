5696f1706e06111901c449dc49fa6a0839477dc0
Isaiah Simon <isimon@okapi.uvm.edu>
2013-12-04 02:59:36 -0500

    Added original files and completed

---
+++ services.php
@@ -0,0 +1,24 @@
+<? include ("top.php"); ?>
+
+<body id="main">
+
+<div id="bgColor" class="rounded-corners border">
+<? include ("header.php"); ?>
+<? include ("nav.php"); ?>
+
+<div id="content">
+	<h3>Software</h3>
+		<p>No matter what program you may need help with, our technicians are trained extensively to help with any issue you may be having. We specialize in many of the most common applications, such as web browsers, Microsoft Office, and Adobe Creative Suite. In addition we are also specifically trained to help with any Operating system issues.</p>
+	<h3>Hardware</h3>
+		<p>Whether you have a Dell, HP, Mac or any other brand, we can assist you with repairing your broken computers hardware. Our hardware certified technicians will replace anything in your computer to get it up and running when you need it.</p>
+	<h3>Security</h3>
+		<p>Afraid your data might be at risk or your accounts have been gotten into. At Quick Computing, our highly trained staff can help you to make sure your computer and accounts are the safest they have ever been and with our extensive training programs, you will learn the dangers of the internet.</p>
+	<h3>Networking</h3>
+		<p>Maybe you just need wireless setup at home or you are trying to setup a small business network, our network consultants are here to help you every step of the way to get your network up and running.</p>
+</div>
+
+<? include ("footer.php"); ?>
+
+</div>
+</body>
+</html>

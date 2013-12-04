ebb0eedc74832db7a938fcf8d30004e12f442300
Isaiah Simon <isimon@okapi.uvm.edu>
2013-12-03 22:11:51 -0500

    Changed wording to let admins know where to delete records

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@
 <section id="content" class="noBorder">
 	<ul>
 		<li><a href="http://www.uvm.edu/~isimon/cs148/assignment7.1/adminAdd.php">Add a new record</a></li>
-		<li><a href="http://www.uvm.edu/~isimon/cs148/assignment7.1/adminSearch.php">Search for an existing record.</a></li>
+		<li><a href="http://www.uvm.edu/~isimon/cs148/assignment7.1/adminSearch.php">Search or Delete an existing record.</a></li>
 	
 </section>
 

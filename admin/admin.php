83690d4c8946e0e1853244a2b54f045201d82859
Isaiah Simon <isimon@okapi.uvm.edu>
2013-12-10 20:24:12 -0500

    Added files for WebAuth files

---
+++ admin/admin.php
@@ -0,0 +1,21 @@
+<? include ("top.php"); ?>
+
+<body id="admin">
+
+<section id="bgColor" class="rounded-corners border">
+<? include ("header.php"); ?>
+<? include ("nav.php"); ?>
+
+<section id="content" class="noBorder">
+	<h2>Admin Home</h2>
+	<ul>
+		<li><a href="adminAdd.php">Add a new record</a></li>
+		<li><a href="adminSearch.php">Search or Delete an existing record.</a></li>
+	</ul>
+</section>
+
+<? include ("footer.php"); ?>
+
+</section>
+</body>
+</html>
---
+++ admin/nav.php
@@ -0,0 +1,10 @@
+<nav class="bgColor">
+<ul>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/main.php'>Home</a></li>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/about.php'>About Us</a></li>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/services.php'>Offered Services</a></li>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/loginRequired/serviceform.php'>Service Form</a></li>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/loginRequired/search.php'>Check Your Issue Status</a></li>
+   <li><a href='https://www.uvm.edu/~isimon/cs148/assignment7.1/admin/admin.php'>Admin Area</a></li>
+</ul>
+</nav>
